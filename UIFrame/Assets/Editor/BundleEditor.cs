eee4ab095ed13cceb9cda035887c7450ba1843be
luomuxie <835657341@qq.com>
2022-03-19 12:34:03 +0800

    AB-保存依赖

--- UIFrame/Assets/Editor/BundleEditor.cs
+++ UIFrame/Assets/Editor/BundleEditor.cs
@@ -7,20 +7,17 @@ public class BundleEditor
 {
     public static string ABCONFIGPATH = "Assets/Editor/ABConfig.asset";
     public static Dictionary<string, string> m_allFileDir = new Dictionary<string,string>();
+    public static List<string> m_allFilesAB = new List<string>();
+    public static Dictionary<string, List<string>> m_allPrefabPaths = new Dictionary<string, List<string>>();
 
     [MenuItem("Tools/打包")]
     public static void Bulid()
     {
         m_allFileDir.Clear();
-        ABConfig config = AssetDatabase.LoadAssetAtPath<ABConfig>(ABCONFIGPATH);
-        foreach (var item in config.m_allPrefebPath)
-        {
-            
-            Debug.Log(item);
-        }
+        m_allFilesAB.Clear();
         ABConfig aBConfig = AssetDatabase.LoadAssetAtPath<ABConfig>(ABCONFIGPATH);
 
-        foreach (ABConfig.FileDirName  vo in config.m_allFileDirName)
+        foreach (ABConfig.FileDirName  vo in aBConfig.m_allFileDirName)
         {
             if (m_allFileDir.ContainsKey(vo.ABname))
             {
@@ -29,17 +26,53 @@ public class BundleEditor
             else
             {
                 m_allFileDir.Add(vo.ABname, vo.Path);
+                m_allFilesAB.Add(vo.Path);
             }
             Debug.Log(vo.ABname +" "+ vo.Path );
         }
-
-        string[] allStr = AssetDatabase.FindAssets("t:Prefab", aBConfig.m_allPrefebPath.ToArray());
+        string[] allStr = AssetDatabase.FindAssets("t:Prefab",null);
         for (int i = 0; i < allStr.Length; i++)
         {
             string path = AssetDatabase.GUIDToAssetPath(allStr[i]);
             EditorUtility.DisplayProgressBar("查找prefab", "prefab:" + path, i * 1.0f / allStr.Length);
+            if (!isContainAllFileAB(path))
+            {
+                GameObject obj = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+                string[] allDepend = AssetDatabase.GetDependencies(path);
+                List<string> allDependPath = new List<string>();
+                allDependPath.Add(path);
+                for (int j = 0; j < allDepend.Length; j++)
+                {
+                    Debug.Log(allDepend[j]);
+                    if(!isContainAllFileAB(allDepend[j]) && !allDepend[j].EndsWith(".cs")){
+                        m_allFilesAB.Add(allDepend[j]);
+                        allDependPath.Add((string)allDepend[j]);
+                    }
+
+                }
+
+                if (m_allPrefabPaths.ContainsKey(obj.name))
+                {
+                    Debug.LogError("已存在相同路径"+obj.name);
+                }
+                else
+                {
+                    m_allPrefabPaths.Add(obj.name, allDependPath);
+                }
+            }
+
         }
-        EditorUtility.ClearProgressBar();
+        EditorUtility.ClearProgressBar();        
+
+    }
+    //是否包含路径
+    static bool isContainAllFileAB(string path)
+    {
+        for (int i = 0; i < m_allFilesAB.Count; i++)
+        {
+            if(m_allFilesAB[i] == path || path.Contains(m_allFilesAB[i])) return true;
+        }
+        return false;
     }
 }    
    
