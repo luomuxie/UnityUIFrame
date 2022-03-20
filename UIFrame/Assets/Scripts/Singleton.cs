13a1978e1f6e6d67d863627e8868caa5693a7951
luomuxie <835657341@qq.com>
2022-03-20 14:44:36 +0800

    资源管理init

---
+++ UIFrame/Assets/Scripts/Singleton.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T>where T : new()
+{
+    private static T m_instance;
+    public static T instance
+    { get
+        {
+            if (m_instance == null)
+            {
+                m_instance = new T();
+            }
+            return m_instance;
+        }
+    }
+}
