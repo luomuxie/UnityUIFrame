1ba7c7b675ee24e908e9f16357e9e6f123fd6854
luomuxie <835657341@qq.com>
2022-03-20 18:59:41 +0800

    资源管理-添加对像池外部使用接口

--- UIFrame/Assets/Scripts/ObjectManager.cs
+++ UIFrame/Assets/Scripts/ObjectManager.cs
@@ -1,8 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class ObjectManager : Singleton<ObjectManager>
 {
-    
+    public ClassObjectPool<ResourceTest> test = ObjectManager.instance.getOrCreateClassPool<ResourceTest>(100);
+
+
+    protected Dictionary<Type, object> m_classPoolDic = new Dictionary<Type, object>();
+
+    public ClassObjectPool<T> getOrCreateClassPool<T>(int cnt) where T:class,new()
+    {
+        Type type = typeof(T);
+        
+        if(!m_classPoolDic.TryGetValue(type, out var outObj) || outObj == null)
+        {
+            ClassObjectPool<T> newPool = new ClassObjectPool<T>(cnt);
+            m_classPoolDic.Add(type, newPool);
+            return newPool;
+        }
+        return outObj as ClassObjectPool<T>;
+    }
+
+    public void testClasee()
+    {
+        test.Spawn(true);
+        
+    }
 }
