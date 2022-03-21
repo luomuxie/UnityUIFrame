6925deb2347f9b022316f6769489e9b8ace530b9
luomuxie <835657341@qq.com>
2022-03-21 09:56:16 +0800

    ResourceManager-构建双向链表

--- UIFrame/Assets/Scripts/ResourceManager.cs
+++ UIFrame/Assets/Scripts/ResourceManager.cs
@@ -7,3 +7,22 @@ public class ResourceManager : Singleton<ResourceManager>
     
 }
 
+public class DoubleLinkListNode<T> where T : class,new ()
+{
+    public DoubleLinkListNode<T> prev = null;
+    public DoubleLinkListNode<T> next = null;
+    public T t = null;
+}
+
+public class DoubleLinkList<T> where T : class,new ()
+{
+    public DoubleLinkListNode<T> Head = null;
+    public DoubleLinkListNode<T> Taild = null;
+    protected ClassObjectPool<DoubleLinkListNode<T>> m_DoubleLinkNodePool = ObjectManager.Instance.getOrCreateClassPool<DoubleLinkListNode<T>>(500);
+    protected int m_Count = 0;
+    public int Count
+    {
+        get { return m_Count; }
+    }
+}
+
