2e705565c24440fbc330345f8eb9eeb8095ae67e
luomuxie <835657341@qq.com>
2022-03-21 11:19:33 +0800

    ResourceManager-添加双向链表增、删操作接口

--- UIFrame/Assets/Scripts/ResourceManager.cs
+++ UIFrame/Assets/Scripts/ResourceManager.cs
@@ -24,5 +24,125 @@ public class DoubleLinkList<T> where T : class,new ()
     {
         get { return m_Count; }
     }
+
+    public DoubleLinkListNode<T> AddToHeader(T t)
+    {
+        DoubleLinkListNode<T> node = m_DoubleLinkNodePool.Spawn(true);
+        //重置对像池对像
+        node.prev = null;
+        node.next = null;
+        return AddToHeader(node);
+    }
+
+    public DoubleLinkListNode<T> AddToHeader(DoubleLinkListNode<T> node )
+    {
+        if(node == null) return null;
+        node.prev = null;
+        if (Head == null)
+        {
+            Head = Taild = node;
+        }
+        else
+        {
+            node.next = Head;
+            Head.prev = node;
+            Head = node;
+        }
+        m_Count++;
+        return node;
+    }
+
+    public DoubleLinkListNode<T> AddToTail(T t)
+    {
+        DoubleLinkListNode<T> node = m_DoubleLinkNodePool.Spawn(true);
+        node.prev = null;
+        node.next = null;
+        return AddToTail(node);
+    }
+
+    public DoubleLinkListNode<T> AddToTail(DoubleLinkListNode<T> node)
+    {
+       if(node==null) return null;
+        if (Taild == null)
+        {
+            Head = Taild = node;
+        }
+        else
+        {
+            node.prev = Taild;
+            Taild.next = node;
+            Taild = node;
+        }
+        m_Count++;
+        return node;
+    }
+
+    /// <summary>
+    /// 移除某节点
+    /// </summary>
+    /// <param name="node"></param>
+    public void RemoveNode(DoubleLinkListNode<T> node)
+    {
+        if (node == null) return;
+        if(node == Head)
+        {
+            Head = node.next;
+        }
+        if(node == Taild)
+        {
+            Taild = node.prev;
+        }
+
+        if(node.prev != null)
+        {
+            node.prev.next = node.next;
+        }
+        if(node.next != null)
+        {
+            node.next.prev = node.prev;
+        }
+
+        node.prev = node.next = null;
+        node.t = null;
+        m_DoubleLinkNodePool.Recycle(node);
+        m_Count--;
+    }
+    /// <summary>
+    /// 把节点移到头部
+    /// </summary>
+    /// <param name="node"></param>
+
+    public void MoveToHead(DoubleLinkListNode<T> node)
+    {
+        if(node == null || node == Head) return;
+        if(node.prev == null || node.next == null) return;
+        if(node == Taild)
+        {
+            Taild = node.prev;
+
+        }
+        if(node.prev != null)
+        {
+            node.prev.next = node.next;
+        }
+        if(node.next != null)
+        {
+            node.next.prev = node.prev;
+        }
+
+        node.prev = null;
+        node.next = Head;
+        Head.prev = node;
+        Head = node;
+        if(Taild == null)
+        {
+            Taild = Head;
+        }
+
+    }
+
+
 }
+
+
 
